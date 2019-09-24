5b9da988b43601f14b564da7af7f9d4604f115a3
Wasif Baliyan <w.baliyan@gmail.com>
2019-09-24 12:00:59 +0530

    Updates

---
+++ precache-manifest.a950497f4d85a254ff5e54d194e86184.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "050ac84d4d90976d55c2e15e08e840de",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "356fe2fce7b4211fdd56",
+    "url": "/monster-rolodex/static/css/main.a1aeaee7.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "356fe2fce7b4211fdd56",
+    "url": "/monster-rolodex/static/js/main.51dadab6.chunk.js"
+  },
+  {
+    "revision": "52b3135f1aa77ef4ddba",
+    "url": "/monster-rolodex/static/js/runtime~main.b7028ca9.js"
+  }
+]);

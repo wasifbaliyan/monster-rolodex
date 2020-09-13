d97038dfe50d20e4fe80489466906e53502f87b9
Wasif Baliyan <w.baliyan@gmail.com>
2020-09-13 10:06:52 +0530

    add react hooks and implement useState and useEffect hooks at their corresponding places

--- src/App.js
+++ src/App.js
@@ -1,56 +1,94 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { CardList } from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-  }
+function App() {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
-  }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+      .then((users) => setMonsters(users));
+  }, [monsters]);
+  const handleChange = (e) => {
+    setSearchField(e.target.value);
   };
-  render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+  const filteredMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
 
-    return (
-      <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearchBox
-          placeholder="Search Monsters"
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters} />
-        <footer className="footer">
-          © {new Date().getFullYear()}, Made with
-          <span
-            style={{ margin: "0 0.3rem" }}
-            role="img"
-            aria-labelledby="heart emoji"
-          >
-            ❤️
-          </span>
-          by
-          {` `}
-          <a href="https://twitter.com/wasifbaliyan">Wasif Baliyan</a>
-        </footer>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder="Search Monsters" handleChange={handleChange} />
+      <CardList monsters={filteredMonsters} />
+      <footer className="footer">
+        © {new Date().getFullYear()}, Made with
+        <span
+          style={{ margin: "0 0.3rem" }}
+          role="img"
+          aria-labelledby="heart emoji"
+        >
+          ❤️
+        </span>
+        by
+        {` `}
+        <a href="https://wasifbaliyan.com">Wasif Baliyan</a>
+      </footer>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => this.setState({ monsters: users }));
+//   }
+
+//   handleChange = (e) => {
+//     this.setState({ searchField: e.target.value });
+//   };
+//   render() {
+//     const { monsters, searchField } = this.state;
+//     const filteredMonsters = monsters.filter((monster) =>
+//       monster.name.toLowerCase().includes(searchField.toLowerCase())
+//     );
+
+//     return (
+//       <div className="App">
+//         <h1>Monster Rolodex</h1>
+//         <SearchBox
+//           placeholder="Search Monsters"
+//           handleChange={this.handleChange}
+//         />
+//         <CardList monsters={filteredMonsters} />
+//         <footer className="footer">
+//           © {new Date().getFullYear()}, Made with
+//           <span
+//             style={{ margin: "0 0.3rem" }}
+//             role="img"
+//             aria-labelledby="heart emoji"
+//           >
+//             ❤️
+//           </span>
+//           by
+//           {` `}
+//           <a href="https://twitter.com/wasifbaliyan">Wasif Baliyan</a>
+//         </footer>
+//       </div>
+//     );
+//   }
+// }
 export default App;
